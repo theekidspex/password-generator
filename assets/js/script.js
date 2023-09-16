310e0cc58b3d4e9a597801c95d7e372ffcc86c1a
theekidspex <theekidspex@gmail.com>
2023-09-16 01:47:49 -0700

    finalized design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var Uppercase = "A, B, C, D, E, F, G, H, I ,J ,K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
 var Lowercase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
 var Specialcharacters = "!,@,#,$,%,^,&,*,(,)"
+var Numeric = "1,2,3,4,5,6,7,8,9,0"
+var password=document.getElementById("password")
+
+
+
+
+
